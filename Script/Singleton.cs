f42828c53e3500d490466a1fe9d28f8630789687
GyutaeLee <gyutae07@gmail.com>
2022-08-14 15:56:23 +0900

    feat : Debug.cs
    DEBUG_MODE 디파인이 존재하는 경우에만 빌드에 포함되되록 예외 처리가 추가된 Debug 관련 클래스를 추가합니다.
    이외에도 범용적인 Debug 관련 기능들을 포함합니다.

---
+++ Script/Singleton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace SupportPackage
+{
+    public class Singleton<T> : MonoBehaviour where T : Component
+    {
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<T>();
+                    if (instance == null)
+                    {
+                        GameObject obj = new GameObject(typeof(T).Name);
+                        instance = obj.AddComponent<T>();
+                    }
+                    return instance;
+                }
+                return instance;
+            }
+            set
+            {
+                instance = value;
+            }
+        }
+    }
+}
